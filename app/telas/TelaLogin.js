607b8741544779c1f017670f74dcb1f8807d5fae
Jessica Cabral <j.pires.cursos@gmail.com>
2025-07-23 18:46:52 -0300

    login e registro: aplicado AsyncStorage

--- app/telas/TelaLogin.js
+++ app/telas/TelaLogin.js
@@ -2,9 +2,40 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Botao from '../components/Botao';
 import Input from '../components/Input';
+import { useState } from 'react';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import TelaRegistro from './TelaRegistro';
+import TelaPrincipal from './TelaPrincipal';
+
 
 export default function TelaLogin() {
   const navigation = useNavigation();
+  const [email, setEmail] = useState ('')
+  const [senha, setSenha] = useState ('')
+
+  async function validarUsuario() {
+    //verifica se o email e a senha foram preenchidos
+    if (!email || !senha) {
+      alert ("Informe o usuário e a senha")
+      return
+    }
+    try {
+
+      const usuarioCadastrado = await AsyncStorage.getItem(email);
+      const usuario = JSON.parse(usuarioCadastrado);
+        if (!usuarioCadastrado) {
+          alert ("Email não cadastrado!");
+          return;
+        };
+        if (usuario.senha === senha){
+          navigation.navigate("TelaPrincipal");
+        } else {
+          alert ("Senha incorreta");
+        } 
+    } catch (error) {
+        alert ("Dados invalidos");
+    }   
+  }
 
   return (
     <View style={styles.container}>
@@ -15,17 +46,18 @@ export default function TelaLogin() {
       />
       <Text style={styles.nomeSistema}>Chama Serviço</Text>
       <Text style={styles.titulo}>Login</Text>
-      <Input placeholder="Email" />
-      <Input placeholder="Senha" secureTextEntry />
+      <Input placeholder="Email" value={email} onChangeText={setEmail}/>
+      <Input placeholder="Senha" secureTextEntry value={senha} onChangeText={setSenha} />
       <Botao 
         titulo="Entrar" 
-        onPress={() => navigation.navigate('TelaPrincipal')} 
+        onPress={validarUsuario} 
       />
       <TouchableOpacity onPress={() => navigation.navigate('TelaRegistro')}>
         <Text style={styles.link}>Não tem conta? Cadastre-se</Text>
       </TouchableOpacity>
     </View>
   );
+
 }
 
 const styles = {
--- app/telas/TelaRegistro.js
+++ app/telas/TelaRegistro.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import Botao from '../components/Botao';
-import Input from '../components/Input';
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Botao from "../components/Botao";
+import Input from "../components/Input";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useState } from "react";
+import TelaLogin from "./TelaLogin";
 
 export default function TelaRegistro() {
   const navigation = useNavigation();
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
+
+  //função cadastrarUsuario
+  async function registrarUsuario() {
+    
+    if (senha === confirmarSenha) {
+      try {
+        const chave = email;
+        const usuario = JSON.stringify({ nome, senha });
+        const usuarioExistente = await AsyncStorage.getItem(email);
+      
+        if (usuarioExistente) {
+        alert("Email já cadastrado!");
+        }
+        await AsyncStorage.setItem(chave, usuario);
+        alert("Usuário cadastrado com sucesso");
+        navigation.navigate("TelaLogin");
+      } catch (error) {
+        alert("Erro ao cadastrar usuário!");
+      }
+    } else {
+      alert("Senhas divergentes!");
+    }
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>Registro</Text>
-      <Input placeholder="Nome Completo" />
-      <Input placeholder="Email" />
-      <Input placeholder="Senha" secureTextEntry />
-      <Input placeholder="Confirmar Senha" secureTextEntry />
-      <Botao 
-        titulo="Criar Conta" 
-        onPress={() => navigation.navigate('TelaPrincipal')} 
+      <Input placeholder="Nome Completo" value={nome} onChangeText={setNome} />
+      <Input placeholder="Email" value={email} onChangeText={setEmail} />
+      <Input
+        placeholder="Senha"
+        secureTextEntry
+        value={senha}
+        onChangeText={setSenha}
       />
-      <TouchableOpacity onPress={() => navigation.navigate('TelaLogin')}>
+      <Input
+        placeholder="Confirmar Senha"
+        secureTextEntry
+        value={confirmarSenha}
+        onChangeText={setConfirmarSenha}
+      />
+      <Botao titulo="Criar Conta" onPress={registrarUsuario} />
+      <TouchableOpacity onPress={() => navigation.navigate("TelaLogin")}>
         <Text style={styles.link}>Já tem conta? Faça login</Text>
       </TouchableOpacity>
     </View>
@@ -28,19 +65,19 @@ export default function TelaRegistro() {
 const styles = {
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     padding: 20,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
   },
   titulo: {
     fontSize: 24,
     marginBottom: 20,
-    textAlign: 'center',
-    color: '#9c88ff',
+    textAlign: "center",
+    color: "#9c88ff",
   },
   link: {
-    color: '#9c88ff',
-    textAlign: 'center',
+    color: "#9c88ff",
+    textAlign: "center",
     marginTop: 15,
   },
 };
